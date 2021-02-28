095e606483cc6c9a81e9da3f71977a640b191553
linhYedda <linh.yedda@yedda.ai>
2021-02-28 10:27:04 +0700

    add log

--- file_transfer/utils/handler.py
+++ file_transfer/utils/handler.py
@@ -1,12 +1,12 @@
 from file_transfer.access.access import get_video_file_name
 from file_transfer.config import IS_TRANSFER_ALL, FROM_DIR, TO_DIR
-from file_transfer.utils.helper import file_transferring
+from file_transfer.utils.helper import file_transferring, file_transferring_all
 
 
 def handle():
     success = None
     if IS_TRANSFER_ALL:
-        success = file_transferring(FROM_DIR, TO_DIR)
+        success = file_transferring_all(FROM_DIR, TO_DIR)
     else:
         list_file = get_video_file_name()
         success = file_transferring(list_file, FROM_DIR, TO_DIR)
