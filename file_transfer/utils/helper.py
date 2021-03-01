1d55cc0df583d13484629a3fe0025cf303ff9bbe
linhYedda <linh.yedda@yedda.ai>
2021-03-01 07:55:55 +0700

    addd handle error

--- file_transfer/utils/helper.py
+++ file_transfer/utils/helper.py
@@ -14,7 +14,9 @@ def file_transferring(file_list, from_dir, to_dir):
         print("Path is invalid")
         return False
 
-    for file_name in file_list:
+    num_file = len(file_list)
+    for idx, file_name in enumerate(file_list):
+        print(f'\r {idx}/{num_file}: {file_name} is tranferring')
         if not os.path.exists(os.path.join(from_dir, file_name)):
             print(f'{file_name} not exist')
         else:
@@ -32,17 +34,25 @@ def file_transferring_all(from_dir, to_dir):
     :param from_dir: string
     :return TRUE or FALSE
     """
-    if not os.path.isdir(to_dir) or not os.path.isdir(from_dir):
-        print("Path is invalid")
+    if not os.path.isdir(from_dir):
+        print(f"{from_dir} is invalid path")
+        return False
+
+    if not os.path.isdir(to_dir):
+        print(f"{to_dir} is invalid path")
         return False
 
     if os.listdir(from_dir).__len__() == 0:
         print("Have no files to transfer")
         return False
 
-    for file_name in os.listdir(from_dir):
+    list_file = os.listdir(from_dir)
+    num_file = len(list_file)
+    for idx, file_name in enumerate(list_file):
+        print(f'\r {idx}/{num_file}: {file_name} is tranferring')
         file_path = os.path.join(from_dir, file_name)
         to_path = os.path.join(to_dir, file_name)
         shutil.move(file_path, to_path)
 
+    print()
     return True
